c71e302129e54af615b0194fb188a4cdf00ec812
joaomariosouzatoledo <142330899+zezimdelas@users.noreply.github.com>
2023-09-06 12:05:10 -0300

    adicionandojscss

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  
+    createCanvas(900, 900);
+    background("black") 
+    
+    
+  } 
+  
+  function draw() { 
+    
+    stroke("blue");
+    
+    fill("red"); 
+    
+    if(mouseIsPressed){ 
+      
+      rect(mouseX, mouseY, 20, 20);
+    } 
+  
+   }
